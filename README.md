8fec22fb337ddcbc28d797cade1ed4c0c4ee2ebc
VedMishra28102006 <166307909+VedMishra28102006@users.noreply.github.com>
2025-05-11 09:20:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+1) Description
+
+A spam email detection app which takes a feature as user input and predicts the email class based on the training dataset.
+
+2) How to run
+
+Install the required libraries, run python3 main.py inside the root folder and open the http://localhost:5000/ url.
+
+3) Dependencies
+
+Libraries required to be installed for this project: flask, pandas, sklearn
+
+4) Courtesy
+
+The data for training used in this app is taken from kaggle whose link is given below:
+
+https://www.kaggle.com/datasets/venky73/spam-mails-dataset
+
---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+  context = {}
+  if request.method == "POST":
+    email = request.form["email"]
+    types = ["ham", "spam"]
+    df = pd.read_csv("spam_ham_dataset.csv")
+    vectorizer = TfidfVectorizer(stop_words="english")
+    X = vectorizer.fit_transform(df["text"])
+    y = df["label_num"]
+    clf = SVC(kernel="linear")
+    clf.fit(X, y)
+    input_data = pd.DataFrame([[email]], columns=["text"])
+    input_data = vectorizer.transform(input_data["text"])
+    predicted_class = clf.predict(input_data)
+    context["prediction"] = types[predicted_class[0]]
+  return render_template("index.html", context=context)
+
+if __name__ == "__main__":
+  host = os.getenv("IP", "0.0.0.0")
+  port = int(os.getenv("PORT", 5000))
+  app.run(host=host, port=port)
